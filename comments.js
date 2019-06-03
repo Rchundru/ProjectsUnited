95c7707b9feb31166570517663186d629f66a3f9
Rchundru <rohitchundru@gmail.com>
2019-06-02 20:44:26 -0500

    Add files via upload

--- comments.js
+++ comments.js
@@ -1,47 +1,34 @@
 function editComment(){
-            var CID= parseInt(document.getElementById("Cid").value);
-            var author = document.getElementById("AUTHOR").value;
-            var status = document.getElementById("STATUS").value;
-            var body = document.getElementById("BODY").value;
-            var timestamp;
+           var CID= parseInt(document.getElementById("Cid").value);
+           var author = document.getElementById("AUTHOR").value;
+           var status = document.getElementById("STATUS").value;
+           var body = document.getElementById("BODY").value;
+           var timestamp;
 
-            $.getJSON('https://csc-394-backend.herokuapp.com/comments/' + CID, function(data) {
-                timestamp = `${data.timestamp}`;
-                 var jsonObj = '{"cid":' + CID  + ','+
-                '"status":' + '"' + status + '"' + ','+
-                '"body":' + '"' + body + '"' + ','+
-                '"timestamp":' + '"' + timestamp + '"' + ',' +
-                '"author":' + '"' + author + '"' +'}';
-            $.ajax({
-                url: "https://csc-394-backend.herokuapp.com/comments/" + CID,
-                contentType: "application/json ",
-                type: "PUT",
-                data: JSON.stringify(jsonObj),
-                success: function(msg){
-                    alert("Success");
-                },
-                error: function(msg){
-                    alert("user not found, please enter a valid username");
-                }
-            });
-                document.getElementById("url").innerHTML = jsonObj;
-            });
+           $.getJSON('https://csc-394-backend.herokuapp.com/comments/' + CID, function(data) {
+               timestamp = `${data.timestamp}`;
+                var jsonObj = {
+                                            "cid":CID ,
+                                            "status":status,
+                                            "body":body,
+                                            "timestamp":timestamp,
+                                            "author":author};
+           $.ajax({
+               url: "https://csc-394-backend.herokuapp.com/comments/" + CID,
+               contentType: "application/json ",
+               type: "PUT",
+               data: JSON.stringify(jsonObj),
+               success: function(msg){
+                   alert("Success");
+               },
+               error: function(msg){
+                   alert("user not found, please enter a valid username");
+               }
+           });
+               document.getElementById("url").innerHTML = JSON.stringify(jsonObj);
+           });
 
-        }
-
-        function deleteComment(){
-            var cid = document.getElementById("CID").value;
-                $.ajax({
-                    url: "https://csc-394-backend.herokuapp.com/comments/" + cid,
-                    type: "DELETE",
-                    success: function(msg){
-                        alert("Comment deleted: " + msg);
-                    },
-                    error: function(msg){
-                        alert(("Unknown comment, please enter a valid CID"))
-                    }
-                });
-        }
+       }
 
         $.getJSON('https://csc-394-backend.herokuapp.com/comments/', function(data) {
             var jsonObjs = JSON.stringify(data);
