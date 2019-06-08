5828c63f0ad883a775c9ed528f1565f919c09e6d
Rchundru <rohitchundru@gmail.com>
2019-06-08 15:54:53 -0500

    Add files via upload

---
+++ prof-admin-projectGrades.js
@@ -0,0 +1,98 @@
+function editGrade(){
+  var username = document.getElementById("uName").value;
+  var project = document.getElementById("PROJECT").value;
+  var grade = document.getElementById("GRADE").value;
+  var responsibility = document.getElementById("RESPONSIBILITY").value;
+  $.getJSON("https://csc-394-backend.herokuapp.com/projectRoster/" + username + "/" + project, function(data) {
+    if(grade === undefined || grade === ""){
+      grade = `${data.grade}`;
+    }
+    if(responsibility === undefined || responsibility === ""){
+      responsibility = `${data.responsibility}`;
+    }
+  var jsonObj = {
+    "responsibility":responsibility,
+    "grade":grade ,
+    "projectRoster":{
+      "project": project,
+      "student":username
+    }};
+    $.ajax({
+      url: "https://csc-394-backend.herokuapp.com/projectRoster/" + username + "/" + project,
+      contentType: "application/json ",
+      type: "PUT",
+      data: JSON.stringify(jsonObj),
+      success: function(msg){
+        alert("Success");
+      },
+      error: function(msg){
+        alert("user not found, please enter a valid username");
+      }
+    });
+  });
+  }
+
+  function addGrade(){
+    var username = document.getElementById("uNamea").value;
+    var assignment = document.getElementById("PROJECTA").value;
+    var responsibility = document.getElementById("RESPONSIBILITYA").value;
+    var grade = document.getElementById("GRADEA").value;
+    var jsonObj = {
+      "responsibility":responsibility,
+      "grade":grade ,
+      "projectRoster":{
+        "project": assignment,
+        "student":username
+      }};
+      $.ajax({
+        url: "https://csc-394-backend.herokuapp.com/projectRoster/",
+        contentType: "application/json ",
+        type: "POST",
+        data: JSON.stringify(jsonObj),
+        success: function(msg){
+          alert("Success");
+        },
+        error: function(msg){
+          alert("user not found, please enter a valid username");
+        }
+      });
+    }
+
+    function deleteGrade(){
+      var username = document.getElementById("uNameD").value;
+      var project = document.getElementById("PROJECTD").value;
+
+      $.ajax({
+        url: "https://csc-394-backend.herokuapp.com/projectRoster/" + username + "/" + project,
+        type: "DELETE",
+        success: function(msg){
+          alert("Success");
+        },
+        error: function(msg){
+          alert("user not found, please enter a valid username");
+        }
+      });
+    }
+
+
+    $.getJSON('https://csc-394-backend.herokuapp.com/projectRoster/', function(data) {
+      var list = "";
+      var table = '<table><tr><th>Username</th><th>First Name</th><th>Last Name</th><th>Project</th><th>Responsibility</th><th>Grade</th></tr>';
+
+      $.getJSON('https://csc-394-backend.herokuapp.com/users/', function(result) {
+        for(i in data){
+          for(j in result){
+            if(data[i].projectRoster.student === result[j].username){
+              list += "<tr><td>" + data[i].projectRoster.student +
+              "</td><td>" + result[j].first_name +
+              "</td><td>" + result[j].last_name +
+              "</td><td>" + data[i].projectRoster.project +
+              "</td><td>" + data[i].responsibility +
+              "</td><td>" + data[i].grade + "</td></tr>";
+            }
+          }
+        }
+        document.getElementById("allGrades").innerHTML = table + list + "</table>";
+      });
+
+    });
